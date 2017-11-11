6012a17915a2f5f6ef349f3b999754bf0aa76938
Edison Alfaro Nuñez <edbie2415@hotmail.com>
2017-11-11 01:31:17 -0500

    pego ejercicios

--- src/practicats.ts
+++ src/practicats.ts
@@ -1,3 +1,13 @@
+/*crear un metodo que reciba un boolean y dos nmeros si es true va multiplicar y si es false va a restar
+crear un metodo que reciba una cadena si la cadena es mayor a 4 letras va decir cadena larga y si es menor va decir cadena corta
+crear un metodo que imprima un array enumerando cada item (el array debe de ser solo de cadenas y no debe estar enumerado ejemplo ["pedro","julio","Tomas"] de 10 datos)
+*/
+
+
+
+
+
+
 
     var numa=100;
     var numb=232;
@@ -11,4 +21,6 @@
 
     suma(numa,numb,numc);
     
+
+
     
