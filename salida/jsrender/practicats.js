eed1c3ac26f581aa670ac8f423742ab582e7af87
Edison Alfaro Nuñez <edbie2415@hotmail.com>
2017-11-11 01:05:07 -0500

    fin del primer ejercicio

--- salida/jsrender/practicats.js
+++ salida/jsrender/practicats.js
@@ -1,13 +1,10 @@
 "use strict";
-var suma01;
-(function (suma01) {
-    var num1 = 100;
-    var num2 = 232;
-    var num3 = 454;
-    function suma(num1, num2, num3) {
-        var resultado = num1 + num2 + num3;
-        var msg = 'Total =' + resultado;
-        alert(msg);
-    }
-    suma01;
-})(suma01 || (suma01 = {}));
+var numa = 100;
+var numb = 232;
+var numc = 454;
+function suma(num1, num2, num3) {
+    var resultado = num1 + num2 + num3;
+    var msg = 'Total =' + resultado;
+    console.log(msg);
+}
+suma(numa, numb, numc);
--- src/practicats.ts
+++ src/practicats.ts
@@ -1,12 +1,14 @@
-namespace suma01{
-    var num1=100;
-    var num2=232;
-    var num3=454;
+
+    var numa=100;
+    var numb=232;
+    var numc=454;
     
     function suma(num1:number,num2:number,num3:number){
         var resultado = num1+num2+num3;
         var msg='Total =' + resultado;
-        alert(msg);
+        console.log(msg);
     }
-    suma01;
-}
+
+    suma(numa,numb,numc);
+    
+    
